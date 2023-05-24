ce7e3e113a7fe3e831f63a5e7555d026fef8d4b0
sudhshet <89556015+sudhshet@users.noreply.github.com>
2023-05-24 15:07:26 -0500

    Update installTools.sh

--- installTools.sh
+++ installTools.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/bash
 # Install kubectl
-sudo curl --silent --location -o /usr/local/bin/kubectl \
-   curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.26.4/2023-05-11/bin/darwin/amd64/kubectl
-sudo chmod +x /usr/local/bin/kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.26.4/2023-05-11/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
+echo 'export PATH=$HOME/bin:$PATH' >> ~/.bash_profile
+kubectl version --short --client
 
 # Update aws cli
 cd ~
