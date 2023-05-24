1e5bfee395ba8a7388dc4ebe0de051a1a888c888
sudhshet <89556015+sudhshet@users.noreply.github.com>
2023-05-24 15:09:28 -0500

    Update installTools.sh

--- installTools.sh
+++ installTools.sh
@@ -4,7 +4,7 @@ curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.26.4/2023-05-11/bin/linu
 chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
 echo 'export PATH=$HOME/bin:$PATH' >> ~/.bash_profile
-kubectl version --short --client
+kubectl version --client
 
 # Update aws cli
 cd ~
